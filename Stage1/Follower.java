593dc537a13bcaf4722ce33278f1a5b93a3ee892
Sebastian Sobarzo Leon <sebastian.sobarzo@usm.cl>
2025-04-20 20:45:10 -0400

    Archivos Stage1

---
+++ Stage1/Follower.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+
+public class Follower extends Subscriber {
+   public Follower(String name, String topicName, PrintStream out) {
+//...
+   }
+   public void update(String message) {
+      //...
+   }
+   private PrintStream out;
+} 
